0cd495c881b30eb6cd396848bfbe3ec809ba854a
Michael Roche <mikeroche138@gmail.com>
2024-03-21 11:49:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(`Ralph`);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(`Bob`);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name) {
+    return cats.concat(name);
+  }
+  
+  function prependCat(name) {
+    return [name].concat(cats);
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
